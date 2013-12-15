5b08aff0470918b6b099c5edfcf0c1c1fdd6eeee
Hakim <hakim@mysociety.org>
2013-12-15 12:46:14 +0000

    Some docs on migrate command
    This isn't laid out in an ideal way, due to impedance between the
    App::Cmd help and dbic-migrate's help, eg.:
    
        bin/manage help migrate   # prints 'description' from below
        bin/manage migrate help   # prints manpage from DBICM

--- lib/MyApp/Management/Command/migrate.pm
+++ lib/MyApp/Management/Command/migrate.pm
@@ -4,6 +4,10 @@ use MyApp;
 use MyApp::Management -command;
 use DBIx::Class::Migration::Script;
 
+sub description {
+    'wrapper around dbic-migrate which handles passing schema in from app config';
+}
+
 sub execute {
     my ($self, $opt, $args) = @_;
 
@@ -16,5 +20,22 @@ sub execute {
         $script->_defaults, 
         schema => $app->model)->run;
 }
+
+=head1 NAME
+
+MyApp::Management::Command::migrate - manage DB migrations
+
+=head1 DESCRIPTION
+
+    bin/manage migrate prepare
+    bin/manage migrate install
+    # etc.
+
+This script is a wrapper around dbic-migrate, but also passes the schema from
+the OX application as default.
+
+    bin/manage migrate help  # to see the options for the script
+
+=cut
 
 1;
